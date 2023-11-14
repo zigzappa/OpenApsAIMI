13bc22ff6d76240df90efbd102b24f69a8f53329
mtr93600 <tellier@pm.me>
2023-11-14 13:54:58 +0100

    xdript source interval 5minutes pour Juggluco

--- plugins/source/src/main/kotlin/app/aaps/plugins/source/XdripSourcePlugin.kt
+++ plugins/source/src/main/kotlin/app/aaps/plugins/source/XdripSourcePlugin.kt
@@ -65,12 +65,23 @@ class XdripSourcePlugin @Inject constructor(
         @Inject lateinit var repository: AppRepository
         @Inject lateinit var dataWorkerStorage: DataWorkerStorage
 
+        companion object {
+            var lastDataTimestamp: Long = 0
+        }
+
         override suspend fun doWorkAndLog(): Result {
             var ret = Result.success()
 
             if (!xdripSourcePlugin.isEnabled()) return Result.success(workDataOf("Result" to "Plugin not enabled"))
             val bundle = dataWorkerStorage.pickupBundle(inputData.getLong(DataWorkerStorage.STORE_KEY, -1))
                 ?: return Result.failure(workDataOf("Error" to "missing input data"))
+            val currentTimestamp = bundle.getLong(Intents.EXTRA_TIMESTAMP, 0)
+            if (currentTimestamp - lastDataTimestamp < 240000) {
+                // Less than 5 minutes has passed since last data processing, ignore this data
+                return Result.success(workDataOf("Result" to "Ignoring data, not enough time passed since last processing"))
+            }
+
+            lastDataTimestamp = currentTimestamp
 
             aapsLogger.debug(LTag.BGSOURCE, "Received xDrip data: $bundle")
             val glucoseValues = mutableListOf<TransactionGlucoseValue>()
