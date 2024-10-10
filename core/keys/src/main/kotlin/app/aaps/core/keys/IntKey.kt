32c60e4aefbccd89b1fafc1518e0a95b6e9e2a21
mtr93600 <tellier@pm.me>
2024-10-10 11:13:36 +0200

    log format

--- core/keys/src/main/kotlin/app/aaps/core/keys/IntKey.kt
+++ core/keys/src/main/kotlin/app/aaps/core/keys/IntKey.kt
@@ -83,5 +83,6 @@ enum class IntKey(
     OApsAIMIBFinterval("key_oaps_aimi_BF_interval", 3, 1, 20, defaultedBySM = true),
     OApsAIMISleepinterval("key_oaps_aimi_sleep_interval", 3, 1, 20, defaultedBySM = true),
     OApsAIMIHighCarbAdjISFFact("key_oaps_aimi_highcarbAdjFact",50,1,500),
-    OApsAIMISnackAdjISFFact("key_oaps_aimi_snackAdjFact",50,1,500)
+    OApsAIMISnackAdjISFFact("key_oaps_aimi_snackAdjFact",50,1,500),
+    OApsAIMIlogsize("key_oaps_aimi_logsize",10,1,20)
 }
