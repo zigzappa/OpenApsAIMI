333d211757c22f178269756487262624f12bf559
mtr93600 <tellier@pm.me>
2025-01-10 11:49:55 +0100

    iob2

--- plugins/aps/src/main/kotlin/app/aaps/plugins/aps/openAPSAIMI/DetermineBasalAIMI2.kt
+++ plugins/aps/src/main/kotlin/app/aaps/plugins/aps/openAPSAIMI/DetermineBasalAIMI2.kt
@@ -1807,6 +1807,7 @@ class DetermineBasalaimiSMB2 @Inject constructor(
         this.iob = iob_data.iob.toFloat()
         if (iob_data.basaliob < 0) {
             iob2 = -iob_data.basaliob.toFloat()+ iob
+            this.iob = iob2
         }
 
         val tick: String = if (glucose_status.delta > -0.5) {
