3b7300b3861ab969163d3ea98d079d7bb5a9badb
mtr93600 <tellier@pm.me>
2025-01-18 17:06:12 +0100

    date

--- plugins/aps/src/main/kotlin/app/aaps/plugins/aps/openAPSAIMI/DetermineBasalAIMI2.kt
+++ plugins/aps/src/main/kotlin/app/aaps/plugins/aps/openAPSAIMI/DetermineBasalAIMI2.kt
@@ -2311,7 +2311,7 @@ class DetermineBasalaimiSMB2 @Inject constructor(
             appendLine("╔${"═".repeat(screenWidth)}╗")
             appendLine(String.format("║ %-${screenWidth}s ║", "AAPS-MASTER-AIMI"))
             appendLine(String.format("║ %-${screenWidth}s ║", "OpenApsAIMI Settings"))
-            appendLine(String.format("║ %-${screenWidth}s ║", "18 Janvier 2025"))
+            appendLine(String.format("║ %-${screenWidth}s ║", "18 January 2025"))
             appendLine("╚${"═".repeat(screenWidth)}╝")
             appendLine()
 
